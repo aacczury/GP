c0e86e3c91954f6e31b72a347d62f7603d6d434d
aacczury <yl931905@gmail.com>
2015-09-14 16:01:24 +0800

    some test

--- featureMatching/Source.cpp
+++ featureMatching/Source.cpp
@@ -10,11 +10,141 @@
 #include "opencv2/features2d/features2d.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/nonfree/features2d.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
 
 using namespace cv;
 
 void readme();
 
+void symmetryTest(
+	const std::vector<std::vector<DMatch> >& matches1,
+	const std::vector<std::vector<DMatch> >& matches2,
+	std::vector<DMatch>& symMatches) {
+	// for all matches image 1 -> image 2
+	for (std::vector<std::vector<DMatch> >::
+		const_iterator matchIterator1 = matches1.begin();
+		matchIterator1 != matches1.end(); ++matchIterator1) {
+		// ignore deleted matches
+		if (matchIterator1->size() < 2)
+			continue;
+		// for all matches image 2 -> image 1
+		for (std::vector<std::vector<DMatch> >::
+			const_iterator matchIterator2 = matches2.begin();
+			matchIterator2 != matches2.end();
+		++matchIterator2) {
+			// ignore deleted matches
+			if (matchIterator2->size() < 2)
+				continue;
+			// Match symmetry test
+			if ((*matchIterator1)[0].queryIdx ==
+				(*matchIterator2)[0].trainIdx &&
+				(*matchIterator2)[0].queryIdx ==
+				(*matchIterator1)[0].trainIdx) {
+				// add symmetrical match
+				symMatches.push_back(
+					DMatch((*matchIterator1)[0].queryIdx,
+					(*matchIterator1)[0].trainIdx,
+					(*matchIterator1)[0].distance));
+				break; // next match in image 1 -> image 2
+			}
+		}
+	}
+}
+int ratioTest(std::vector<std::vector<DMatch>> &matches) {
+	float ratio = 0.65f;
+	int removed = 0;
+	// for all matches
+	for (std::vector<std::vector<DMatch> >::iterator
+		matchIterator = matches.begin();
+		matchIterator != matches.end(); ++matchIterator) {
+		// if 2 NN has been identified
+		if (matchIterator->size() > 1) {
+			// check distance ratio
+			if ((*matchIterator)[0].distance /
+				(*matchIterator)[1].distance > ratio) {
+				matchIterator->clear(); // remove match
+				removed++;
+			}
+		}
+		else { // does not have 2 neighbours
+			matchIterator->clear(); // remove match
+			removed++;
+		}
+	}
+	return removed;
+}
+cv::Mat ransacTest(
+	const std::vector<cv::DMatch>& matches,
+	const std::vector<cv::KeyPoint>& keypoints1,
+	const std::vector<cv::KeyPoint>& keypoints2,
+	std::vector<cv::DMatch>& outMatches) {
+	bool refineF = true;
+	double distance = 3.0;
+	double confidence = 0.99;
+	// Convert keypoints into Point2f
+	std::vector<cv::Point2f> points1, points2;
+	cv::Mat fundemental;
+	for (std::vector<cv::DMatch>::
+		const_iterator it = matches.begin();
+		it != matches.end(); ++it) {
+		// Get the position of left keypoints
+		float x = keypoints1[it->queryIdx].pt.x;
+		float y = keypoints1[it->queryIdx].pt.y;
+		points1.push_back(cv::Point2f(x, y));
+		// Get the position of right keypoints
+		x = keypoints2[it->trainIdx].pt.x;
+		y = keypoints2[it->trainIdx].pt.y;
+		points2.push_back(cv::Point2f(x, y));
+	}
+	// Compute F matrix using RANSAC
+	std::vector<uchar> inliers(points1.size(), 0);
+	if (points1.size()>0 && points2.size()>0){
+		cv::Mat fundemental = cv::findFundamentalMat(
+			cv::Mat(points1), cv::Mat(points2), // matching points
+			inliers,       // match status (inlier or outlier)
+			CV_FM_RANSAC, // RANSAC method
+			distance,      // distance to epipolar line
+			confidence); // confidence probability
+		// extract the surviving (inliers) matches
+		std::vector<uchar>::const_iterator
+			itIn = inliers.begin();
+		std::vector<cv::DMatch>::const_iterator
+			itM = matches.begin();
+		// for all matches
+		for (; itIn != inliers.end(); ++itIn, ++itM) {
+			if (*itIn) { // it is a valid match
+				outMatches.push_back(*itM);
+			}
+		}
+		if (refineF) {
+			// The F matrix will be recomputed with
+			// all accepted matches
+			// Convert keypoints into Point2f
+			// for final F computation
+			points1.clear();
+			points2.clear();
+			for (std::vector<cv::DMatch>::
+				const_iterator it = outMatches.begin();
+				it != outMatches.end(); ++it) {
+				// Get the position of left keypoints
+				float x = keypoints1[it->queryIdx].pt.x;
+				float y = keypoints1[it->queryIdx].pt.y;
+				points1.push_back(cv::Point2f(x, y));
+				// Get the position of right keypoints
+				x = keypoints2[it->trainIdx].pt.x;
+				y = keypoints2[it->trainIdx].pt.y;
+				points2.push_back(cv::Point2f(x, y));
+			}
+			// Compute 8-point F from all accepted matches
+			if (points1.size()>0 && points2.size()>0){
+				fundemental = cv::findFundamentalMat(
+					cv::Mat(points1), cv::Mat(points2), // matches
+					CV_FM_8POINT); // 8-point method
+			}
+		}
+	}
+	return fundemental;
+}
 /**
 * @function main
 * @brief Main function
@@ -32,7 +162,9 @@ int main(int argc, char** argv)
 	//-- Step 1: Detect the keypoints using SURF Detector
 	int minHessian = 400;
 
-	SurfFeatureDetector detector(minHessian);
+	OrbFeatureDetector detector(minHessian);
+	//SurfFeatureDetector detector(minHessian);
+	//SiftFeatureDetector detector(minHessian);
 
 	std::vector<KeyPoint> keypoints_1, keypoints_2;
 
@@ -51,19 +183,34 @@ int main(int argc, char** argv)
 
 	//-- Step 2: Calculate descriptors (feature vectors)
 	//SurfDescriptorExtractor extractor;
-	//SiftDescriptorExtractor extractor;
-	OrbDescriptorExtractor extractor;
+	SiftDescriptorExtractor extractor;
+	//OrbDescriptorExtractor extractor;
 
 	Mat descriptors_1, descriptors_2;
 
 	extractor.compute(img_1, keypoints_1, descriptors_1);
 	extractor.compute(img_2, keypoints_2, descriptors_2);
 
-	Ptr<DescriptorMatcher> matcher;
 	//-- Step 3: Matching descriptor vectors using FLANN matcher
-	// FlannBasedMatcher matcher;
+	FlannBasedMatcher matcher;
 	std::vector< DMatch > matches;
-	//matcher.match(descriptors_1, descriptors_2, matches);
+	matcher.match(descriptors_1, descriptors_2, matches);
+
+	/*Ptr<DescriptorMatcher> matcher;
+	std::vector<std::vector< DMatch >> matches1;
+	std::vector<std::vector< DMatch >> matches2;
+	matcher->knnMatch(descriptors_1, descriptors_2, matches1, 2);
+	matcher->knnMatch(descriptors_2, descriptors_1, matches2, 2);
+
+	ratioTest(matches1);
+	ratioTest(matches2);
+
+	std::vector<DMatch> symMatches;
+	symmetryTest(matches1, matches2, symMatches);
+
+	std::vector<cv::DMatch> matches;
+	imshow("Good Matches", ransacTest(symMatches, keypoints_1, keypoints_2, matches));
+	*/
 
 	double max_dist = 0; double min_dist = 100;
 
@@ -86,7 +233,7 @@ int main(int argc, char** argv)
 
 	for (int i = 0; i < descriptors_1.rows; i++)
 	{
-		if (matches[i].distance <= max(1.5 * min_dist, 0.02))
+		if (matches[i].distance <= max(2 * min_dist, 0.02))
 		{
 			good_matches.push_back(matches[i]);
 		}
@@ -106,7 +253,7 @@ int main(int argc, char** argv)
 	{
 		printf("-- Good Match [%d] Keypoint 1: %d  -- Keypoint 2: %d  \n", i, good_matches[i].queryIdx, good_matches[i].trainIdx);
 	}
-
+	
 	waitKey(0);
 
 	return 0;
